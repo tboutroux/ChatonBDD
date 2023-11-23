4b5fd516b9a543625699671f84434ef919f40f8e
tboutroux <tetetoto@PC-de-TETETOTO.localdomain>
2023-11-23 20:25:55 +0100

    Ajout Formulaires + Connexion BDD

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoriesController extends AbstractController
+{
+    #[Route('/categories', name: 'app_categories')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+
+        $repo = $doctrine->getRepository(Categorie::class);
+        $categories = $repo->findAll();
+
+        return $this->render('categories/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    //action pour ajouter une categorie
+    #[Route('/categories/ajouter', name: 'app_categories_ajouter')]
+    public function ajouter(Request $request, ManagerRegistry $doctrine): Response
+    {
+        //créer un formulaire
+        //on crée un objet de la classe Categorie
+        $categorie = new Categorie();
+        //on crée un formulaire
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        //todo : traiter le formulaire en retour
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //on récupère le manager de doctrine
+            $em = $doctrine->getManager();
+            //on demande au manager de sauvegarder l'objet
+            $em->persist($categorie);
+            //on exécute la requête
+            $em->flush();
+
+            //on redirige vers la liste des catégories
+            return $this->redirectToRoute('app_categories');
+        }
+
+        return $this->render('categories/ajouter.html.twig', [
+            "formulaire"=>$form->createView(),
+            ]);
+    }
+}
---
+++ src/Form/CategorieType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; //pour le bouton
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Titre')
+            ->add('Description')
+            ->add('OK', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-dark'],
+                'label'=> 'Ajouter',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
